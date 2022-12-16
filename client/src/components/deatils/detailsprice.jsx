864b8d297f1b27d52efbf77bed525ba56c76d4dd
damanjot6767 <damanjot6767@gmail.com>
2022-12-16 19:57:20 +0530

    add in it

--- client/src/components/deatils/detailsprice.jsx
+++ client/src/components/deatils/detailsprice.jsx
@@ -2,27 +2,27 @@ import React from 'react'
 import { BiCheck} from 'react-icons/bi'
 import "./detailsprice.css"
 
-const Detailsprice = () => {
+const Detailsprice = ({name}) => {
   return (
     <div name="Price" className='detailsprice'>
-        <h1 style={{fontSize:"150%",color:"rgb(96,96,96)",marginLeft:"65px",lineHeight:"60px"}}>{`${`Gmail`} Pricing`}</h1>
-        <p style={{fontSize:"14px",marginLeft:"65px",lineHeight:"50px"}}>{`This integration requires one of the following ${"Gmail"} software subscription plans.`}</p>
+        <h1 style={{fontSize:"150%",color:"rgb(96,96,96)",marginLeft:"65px",lineHeight:"60px"}}>{`${name} Pricing`}</h1>
+        <p style={{fontSize:"14px",marginLeft:"65px",lineHeight:"50px"}}>{`This integration requires one of the following ${name} software subscription plans.`}</p>
         <div className='price-main'>
           
             <div className='details-price1'>
                 <div className='details-price1a' >
                 <h1 style={{fontSize:"150%",lineHeight:"40px"}}>Free</h1>
-                <p>{`Base cost for ${"Gmail"} domain address is Free. Custom domains start at $6 per active user/month`}</p>
+                <p>{`Base cost for ${name} domain address is Free. Custom domains start at $6 per active user/month`}</p>
                 </div>
                  <div className='details-price1b'>
                 <h1 style={{fontSize:"220%",color:"black",lineHeight:"60px",fontWeight:"660",textAlign:"center"}}>Free</h1>
                     <div style={{display:"flex",width:"80%",lineHeight:"40px",margin:"auto"}}>
                       <span style={{position:"absolute",marginTop:"6px"}}><BiCheck size={25} /></span>
-                      <span style={{marginLeft:"25px"}}>{`Send and receive ${"Gmails"}`}</span>
+                      <span style={{marginLeft:"25px"}}>{`Send and receive ${name}`}</span>
                     </div>
                     <div style={{display:"flex",width:"80%",lineHeight:"40px",margin:"auto"}}>
                       <span style={{position:"absolute"}}><BiCheck size={25} /></span>
-                      <span style={{marginLeft:"25px",lineHeight:"22px"}}>{`Access ${"Google Gmail"}, Google Drive, and other features`}</span>
+                      <span style={{marginLeft:"25px",lineHeight:"22px"}}>{`Access ${name}, Google Drive, and other features`}</span>
                     </div>
                 </div>
             </div>
@@ -31,18 +31,18 @@ const Detailsprice = () => {
                
                 <div className='details-price1a'>
                 <h1 style={{fontSize:"150%",lineHeight:"40px"}}>Basic</h1>
-                <p>{`Base cost for ${"Gmail"} domain address is Free. Custom domains start at $6 per active user/month`}</p>
+                <p>{`Base cost for ${name} domain address is Free. Custom domains start at $6 per active user/month`}</p>
                 </div>
                 
                  <div className='details-price1b'>
                  <h1 style={{fontSize:"220%",color:"black",lineHeight:"60px",fontWeight:"660",textAlign:"center"}}>$6</h1>
                     <div style={{display:"flex",width:"80%",lineHeight:"40px",margin:"auto"}}>
                       <span style={{position:"absolute",marginTop:"6px"}}><BiCheck size={25} /></span>
-                      <span style={{marginLeft:"25px"}}>{`Send and receive ${"Gmails"}`}</span>
+                      <span style={{marginLeft:"25px"}}>{`Send and receive ${name}`}</span>
                     </div>
                     <div style={{display:"flex",width:"80%",lineHeight:"40px",margin:"auto"}}>
                       <span style={{position:"absolute"}}><BiCheck size={25} /></span>
-                      <span style={{marginLeft:"25px",lineHeight:"22px"}}>{`Access ${"Google Gmail"}, Google Drive, and other features`}</span>
+                      <span style={{marginLeft:"25px",lineHeight:"22px"}}>{`Access ${name}, Google Drive, and other features`}</span>
                     </div>
                     <div style={{display:"flex",width:"80%",lineHeight:"40px",margin:"auto",marginTop:"6px"}}>
                       <span style={{position:"absolute"}}><BiCheck size={25} /></span>
@@ -55,18 +55,18 @@ const Detailsprice = () => {
                 
                 <div className='details-price1a' >
                 <h1 style={{fontSize:"150%",lineHeight:"40px"}}>Business</h1>
-                <p>{`Base cost for ${"Gmail"} domain address is Free. Custom domains start at $6 per active user/month`}</p>
+                <p>{`Base cost for ${name} domain address is Free. Custom domains start at $6 per active user/month`}</p>
                 </div>
                
                  <div className='details-price1b'>
                  <h1 style={{fontSize:"220%",color:"black",lineHeight:"60px",fontWeight:"660",textAlign:"center"}}>$12</h1>
                     <div style={{display:"flex",width:"80%",lineHeight:"40px",margin:"auto"}}>
                       <span style={{position:"absolute",marginTop:"6px"}}><BiCheck size={25} /></span>
-                      <span style={{marginLeft:"25px"}}>{`Send and receive ${"Gmails"}`}</span>
+                      <span style={{marginLeft:"25px"}}>{`Send and receive ${name}`}</span>
                     </div>
                     <div style={{display:"flex",width:"80%",lineHeight:"40px",margin:"auto"}}>
                       <span style={{position:"absolute"}}><BiCheck size={25} /></span>
-                      <span style={{marginLeft:"25px",lineHeight:"22px"}}>{`Access ${"Google Gmail"}, Google Drive, and other features`}</span>
+                      <span style={{marginLeft:"25px",lineHeight:"22px"}}>{`Access ${name}, Google Drive, and other features`}</span>
                     </div>
                     <div style={{display:"flex",width:"80%",lineHeight:"40px",margin:"auto",marginTop:"6px"}}>
                       <span style={{position:"absolute"}}><BiCheck size={25} /></span>
